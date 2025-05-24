6c2045ccbbb10d311963297d198357426418c7c2
popadina varvara <popadinavar@ya.ru>
2025-05-24 20:58:37 +0300

    workaem

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,5 @@
-import "@/styles"
-
+// src/index.jsx
+import { AssistantProvider } from "@/providers/AssistantProvider"
 import Header from "@/layouts/Header"
 import Menu from "@/components/Menu"
 
@@ -7,11 +7,11 @@ export const metadata = {
   title: "Главная",
 }
 
-export default function () {
+export default function Home() {
   return (
-    <>
+    <AssistantProvider>
       <Header />
       <Menu />
-    </>
+    </AssistantProvider>
   )
 }
